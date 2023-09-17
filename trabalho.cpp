96edcb6a99d7ba27dcddc85b349ecbc69a79e444
yusioka <100078321+guhkadoo@users.noreply.github.com>
2023-09-17 20:12:07 -0300

    mudancona

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,21 +1,15 @@
-#include <ncurses.h>
-#include <cstring>
-#include <string>
-#include <vector>
+#include "entrada.hpp"
+#include "sei_la.hpp"
 
 using namespace std; // Adicionando a diretiva
-using FunctionPtr = void (*)(int);
-
-vector<string> vetorDeStrings;
-int highlight = 0;
-
-void menu(vector<string> menuOptions, string title, int numFunctions, ...);
+                    
 void max_min();
 void forma_padrao();
 
 int main() {
+    setlocale(LC_ALL, "");
     initscr();
-//    noecho();
+    noecho();
     cbreak();
     curs_set(0); // Esconde o cursor
     keypad(stdscr, true);
@@ -27,69 +21,17 @@ int main() {
     return 0;
 }
 
-void menu(vector<string> menuOptions, string title, int numFunctions, ...) {
+void max_min() {
+    echo();
+    clear();
     int height, width;
     getmaxyx(stdscr, height, width);
-    int choice;
-
-    // Crie um vetor de ponteiros para função
-    vector<FunctionPtr> functions;
-    va_list args;
-    va_start(args, numFunctions);
-    for (int i = 0; i < numFunctions; i++) {
-        FunctionPtr func = va_arg(args, FunctionPtr);
-        functions.push_back(func);
-    }
-    va_end(args);
-
-    while (1) {
-        clear();
-        mvprintw(1, (width - title.size()) / 2, title.c_str());
-
-        // Imprime o menu
-        for (int i = 0; i < menuOptions.size(); i++) {
-            if (i == highlight)
-                attron(A_REVERSE); // Destaca a opção selecionada
-            mvprintw(height / 2 + i, (width - menuOptions[i].length()) / 2, menuOptions[i].c_str());
-            attroff(A_REVERSE);
-        }
-
-        // Lê a entrada do usuário
-        choice = getch();
-        switch (choice) {
-            case KEY_UP:
-                highlight--;
-                if (highlight == -1)
-                    highlight = menuOptions.size() - 1;
-                break;
-            case KEY_DOWN:
-                highlight++;
-                if (highlight == menuOptions.size())
-                    highlight = 0;
-                break;
-            case 10: // Enter
-                if (highlight == menuOptions.size() - 1)
-                    goto end;
-                for (int i = 0; i < menuOptions.size() - 1; i++) {
-                    if (highlight == i) {
-                        // Chama a função correspondente
-                        functions[i](highlight);
-                    }
-                }
-                break;
-        }
-    }
-end:
-    highlight = 0;
-    endwin();
-}
-
-void max_min() {
-    echo(); int height, width;
-    clear();
-    getmaxyx(stdscr, height, width);
     char str[100], input[100];
+    int variable_quantity;
     mvprintw(0, 0, "<ESC> para parar de inserir equacoes");
+    mvprintw(2, 0, "Digite a quantidade de variaveis (xn): ");
+    mvscanw(2, 40, "%d", &variable_quantity); 
+    delete_line(stdscr, 2);
     if(highlight == 0) {
         mvprintw(2, 0, "max ");
     }
@@ -110,17 +52,16 @@ void max_min() {
             break;
         }
         else {
-            wmove(stdscr, i+1, 0);
-            wclrtoeol(stdscr);
-            refresh();
+            delete_line(stdscr, i+1);
         }
         strcpy(input, "");
         i++;
     }
-    // Apos ler as infos:
-    clear();
-    vector<vector<int>> matriz_A;
-    // Colocar matriz A, vetor C, vetor B, vetor X.
+    vector<double> c_vector = z_to_c(str);	
+    pair<vector<vector<double>>, vector<double>> matA_vetb = matriz_a_b(vetorDeStrings, variable_quantity);
+    print_vector(c_vector, "C");
+    print_vector(matA_vetb.second, "b");
+    vetorDeStrings.clear();
 }
 
 void forma_padrao() {
@@ -129,6 +70,6 @@ void forma_padrao() {
     clear();
     vector<string> options = {"max", "min", "sair"};
     string title = "";
-    menu(options, title, 2, max_min);
+    menu(options, title, 2, max_min, max_min);
     getch();
 }
